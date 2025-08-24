82284f9d063930739684808a5e7cdbc762ea7655
Piyyaaa <apriyantojordy7@gmail.com>
2025-08-25 01:36:41 +0700

    Update ReviewCard.jsx

--- src/layouts/ReviewCard.jsx
+++ src/layouts/ReviewCard.jsx
@@ -6,14 +6,12 @@ const ReviewCard = (props) => {
   return (
     <div className='flex flex-col w-full lg:w-2/6 bg-white p-3 rounded-lg gap-5'>
         <div className='flex flex-row items-center lg:justify-start justify-center'>
-            <div className='2-1/4'>
+            <div className='w-1/4'>
                 <img className='rounded-full w-16 h-16 object-cover' src={props.img} alt='img'/>
             </div>
             <div className='mx-3'>
                 <h2 className='font-semibold text-lg'>{props.title}</h2>
                 <div className='flex'>
-                    <BsStarFill className='text-brightColor'/>
-                    <BsStarFill className='text-brightColor'/>
                     <BsStarFill className='text-brightColor'/>
                     <BsStarFill className='text-brightColor'/>
                     <BsStarFill className='text-brightColor'/>
