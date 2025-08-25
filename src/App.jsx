b8258e4408dbc6f3dbd80395e33ffb83b9801f35
Piyyaaa <apriyantojordy7@gmail.com>
2025-08-25 17:05:24 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About'
 import Menu from './components/Menu';
-import Review from './components/Review';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
@@ -18,10 +18,8 @@ const App = () => {
         <div id='menu'>
           <Menu/>
         </div>
-        <div id='review'>
-          <Review/>
-        </div>
       </main>
+      <Footer/>
     </div>
   )
 }
