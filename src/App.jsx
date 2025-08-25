e33e0db41609e3560608e4ceeacc306093eca043
Piyyaaa <apriyantojordy7@gmail.com>
2025-08-25 17:45:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About'
 import Menu from './components/Menu';
-import Footer from './components/Footer';
 const App = () => {
   return (
     <div>
